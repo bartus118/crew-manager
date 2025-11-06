b678e480c026e098d86d17451db8643ece98b749
bartus118 <bartus118@gmail.com>
2025-11-06 21:50:40 +0100

    Prawidłowe klucze suprabase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // KONFIGURACJA SUPABASE
-const SUPABASE_URL = 'https://TWÓJ_PROJEKT.supabase.co';
-const SUPABASE_ANON_KEY = 'TWÓJ_PUBLICZNY_ANON_KEY';
+const SUPABASE_URL = 'https://vuptrwfxgirrkvxkjmnn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1cHRyd2Z4Z2lycmt2eGtqbW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NDM3NjUsImV4cCI6MjA3ODAxOTc2NX0.0hLoti7nvGQhQRsrKTt1Yy_cr5Br_XeAHsPdpAnG7NY';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const ADMIN_PASSWORD = "admin123";
