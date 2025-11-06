97a1e660482bf68db314e40953cc47824947efbc
bartus118 <bartus118@gmail.com>
2025-11-06 22:09:28 +0100

    SUPRA LOG

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // -----------------------------
 
 // KONFIGURACJA SUPABASE — Wklej swoje wartości (po utworzeniu projektu Supabase)
-const SUPABASE_URL = 'https://TWÓJ_PROJEKT.supabase.co';
-const SUPABASE_ANON_KEY = 'TWÓJ_PUBLICZNY_ANON_KEY';
+const SUPABASE_URL = 'https://vuptrwfxgirrkvxkjmnn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1cHRyd2Z4Z2lycmt2eGtqbW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NDM3NjUsImV4cCI6MjA3ODAxOTc2NX0.0hLoti7nvGQhQRsrKTt1Yy_cr5Br_XeAHsPdpAnG7NYY';
 
 // Admin
 const ADMIN_PASSWORD = "admin123";
